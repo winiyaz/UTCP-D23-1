d0cba98865c6625fc6c51e1fa100ffcd93ba6b0f
winiyaz <winiyaz@proton.me>
2024-07-27 09:05:30 -0700

    😁Smellherfart - Car generated

--- ver1/car_manager.py
+++ ver1/car_manager.py
@@ -13,13 +13,15 @@ class CarManager:
 		self.all_cars = []
 
 	def create_car(self):
-		new_car = Turtle("square")
-		new_car.shapesize(stretch_wid=1, stretch_len=2)
-		new_car.penup()
-		new_car.color(rp.choice(NEONC))
-		random_y = rp.randint(-250, 250)
-		new_car.goto(300, random_y)
-		self.all_cars.append(new_car)
+		random_chance = rp.randint(1, 6)
+		if random_chance == 1:
+			new_car = Turtle("square")
+			new_car.shapesize(stretch_wid=1, stretch_len=2)
+			new_car.penup()
+			new_car.color(rp.choice(NEONC))
+			random_y = rp.randint(-200, 200)
+			new_car.goto(300, random_y)
+			self.all_cars.append(new_car)
 
 	def move_car(self):
 		for car in self.all_cars:
